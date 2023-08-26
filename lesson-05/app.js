7982747231d0e77ad8e81bdf7d8ed47f3019d925
KarinaCor <mksmvkarina@gmail.com>
2023-08-26 16:12:54 +0300

    commit

--- lesson-05/app.js
+++ lesson-05/app.js
@@ -75,6 +75,63 @@ ulElement.addEventListener('click', updateActivePage);
   |============================
 */
 
+const openModalBtn = document.querySelector(`.js-modal-open`)
+openModalBtn.addEventListener(`click`, openModelBtnClick)
+const modal = basicLightbox.create(`<div class="modal">
+<button type="button" class="close-btn js-modal-close">
+  X
+</button>
+
+<form class="login-form js-modal__form">
+  <label>
+    Name
+    <input type="text" name="name" placeholder="enter your name" />
+  </label>
+  <label>
+    Email
+    <input type="email" name="email" placeholder="enter your email" />
+  </label>
+  <label>
+    Password
+    <input
+      type="password"
+      name="password"
+      placeholder="enter your password"
+    />
+  </label>
+  <button class="login-btn" type="submit">
+    Login
+  </button>
+</form>
+</div>`,{
+  
+  onShow:()=>{
+    document.body.style.overflow = `hidden`;
+    document.addEventListener(`keydown`, onEscapeClick)
+    
+  },
+  onClose:() => {
+    document.body.style.overflow = `visible`;
+    document.removeEventListener(`keydown`, onEscapeClick)
+    document.querySelector(`.js-modal-close`).removeEventListener(`click`, crossClose)
+  }
+})
+  
+function crossClose(event){
+  modal.close()
+}
+
+function openModelBtnClick(event) {
+  modal.show()
+  document.querySelector('.js-modal-close').addEventListener(`click`, crossClose)
+   
+}
+function onEscapeClick(event) {
+  if(event.code !== 'Escape') return;
+  modal.close()
+}
+
+
 /**
   |============Розмітка для модального вікна================
   <div class="modal">
